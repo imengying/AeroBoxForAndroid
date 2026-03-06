276b429491285367819ba28fff552d490b328999
imengying <yuzucv@gmail.com>
2026-03-06 10:35:29 +0800

    调试

--- app/src/main/kotlin/com/aerobox/ui/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/aerobox/ui/screens/HomeScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
