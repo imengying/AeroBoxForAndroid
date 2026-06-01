f3ba31401eaba48974712e560044515d64edbbb1
imengying <yuzucv@gmail.com>
2026-06-02 02:27:02 +0800

    更新依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,7 +93,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.activity:activity-compose:1.13.0")
 
-    implementation(platform("androidx.compose:compose-bom:2026.04.01"))
+    implementation(platform("androidx.compose:compose-bom:2026.05.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-core")
@@ -111,7 +111,7 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
     implementation("org.yaml:snakeyaml:2.6")
     implementation("org.tukaani:xz:1.12")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     val kotlinVersion = "2.3.21"
-    val kspVersion = "2.3.7"
+    val kspVersion = "2.3.9"
 
     repositories {
         google()
@@ -17,7 +17,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "9.2.0" apply false
+    id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
-    id("com.google.devtools.ksp") version "2.3.7" apply false
+    id("com.google.devtools.ksp") version "2.3.9" apply false
 }
